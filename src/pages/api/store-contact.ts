4299605d9ab617e59e1f3587195242c0f0145013
Chunyang Shen <cshen@cshen-macbookpro.local>
2023-06-17 18:19:46 -0700

    Fix

---
+++ src/pages/api/store-contact.ts
@@ -0,0 +1,38 @@
+import { marshall } from "@aws-sdk/util-dynamodb";
+import {
+  DynamoDBClient,
+  PutItemCommand,
+} from '@aws-sdk/client-dynamodb';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+const client = new DynamoDBClient({});
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+  ) {
+    const { email, name, tier, message } = req.body;
+    const currentTime = new Date();
+    const Item = {
+        email,
+        name,
+        tier,
+        message,
+        time: currentTime.toISOString(),
+    };
+    console.log(Item)
+    try {
+      await client.send(
+        new PutItemCommand({
+          TableName: process.env.TABLE_NAME,
+          Item: marshall(Item),
+        })
+      );
+  
+      return res.status(200).json({ status: 'success' });
+    } catch (error) {
+      console.error('Error occurred while writing to DynamoDB:', error);
+      return res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+    }
+}
