7704e56119c1044922c985b33d01bd59758ae3b5
Chunyang Shen <cshen@cshen-macbookpro.local>
2023-06-27 12:07:44 -0700

    Update waitlist

--- src/components/waitlist.jsx
+++ src/components/waitlist.jsx
@@ -14,11 +14,13 @@
 */
 import { useRouter } from 'next/navigation'
 import { useForm, SubmitHandler, FieldValues } from 'react-hook-form';
+import { useState } from 'react'
 
 
 export default function WaitList({ tier }) {
 
   const { register, handleSubmit, reset } = useForm();
+  const [isSucceed, setIsSucceed] = useState(false)
   const router = useRouter()
 
   const onSubmit = async ({ firstname, lastname, company, email, phonenumber, message }) => {
@@ -56,9 +58,9 @@ export default function WaitList({ tier }) {
     } catch (error) {
       console.error('Error occurred while calling /api/store-contact:', error);
     } 
-    
-
     reset();
+    setIsSucceed(true)
+    await new Promise(r => setTimeout(r, 1500));
     router.push('/')
   };
 
@@ -83,6 +85,11 @@ export default function WaitList({ tier }) {
           The product is ready to launch soon, join our first tier user group for free trail and help us improve the features
         </p>
       </div>
+      {isSucceed&&(
+      <div className="mx-auto bg-teal-100 max-w-2xl border-t border-b border-teal-500 text-teal-900 px-4 py-3 text-center" role="alert">
+        <p className="text-sm">You have successfully joined our waitlist!</p>
+      </div>)}
+
       <form className="mx-auto mt-16 max-w-xl sm:mt-20" onSubmit={handleSubmit(onSubmit)}>
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
@@ -123,7 +130,7 @@ export default function WaitList({ tier }) {
                 id="company"
                 autoComplete="organization"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                {...register('company', { required: false })}
+                {...register('company', { required: true })}
               />
             </div>
           </div>
